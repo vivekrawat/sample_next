eab6bb166413f6d50b7e9772166e8170bc7a98d4
vivekrawat <vivekrawat7534@gmail.com>
2024-04-03 16:03:59 +0530

    feat/addded-pagination-to-invoices0

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,6 +4,7 @@ import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
 import { Suspense } from 'react';
  
 export default async function Page({
@@ -16,6 +17,9 @@ export default async function Page({
   }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+  // const totalPages = 0
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -29,7 +33,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
