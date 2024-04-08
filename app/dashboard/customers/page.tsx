bd20081dc752d2d9d79c06aba2d2dabe56e614d2
vivekrawat <vivekrawat7534@gmail.com>
2024-04-08 22:51:46 +0530

    feat/added-search-functionality-to-cusomers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,12 @@
 import Table from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from '@/app/lib/data';
-export default async function Page() {
-  const customers = await fetchFilteredCustomers('');
+export default async function Page({searchParams}: {
+    searchParams?: {
+        query?:string 
+    }
+}) {
+  const query =  searchParams?.query ? searchParams.query : ''
+  const customers = await fetchFilteredCustomers(query);
   console.log(customers)
   return (
     <div className="w-full">
